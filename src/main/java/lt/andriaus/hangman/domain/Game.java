fe2c0f2d7935d01ab610d5f4ae4c87528563c14a
Andrius Simanaitis <andrius@bei.lt>
2021-02-02 15:59:15 +0200

    added Game entity

---
+++ src/main/java/lt/andriaus/hangman/domain/Game.java
@@ -0,0 +1,63 @@
+package lt.andriaus.hangman.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+
+public class Game {
+    private final String word;
+    private final List<Character> letters;
+
+    private Game(String word, List<Character> letters) {
+        this.word = word;
+        this.letters = letters;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public Game guessLetter(char letter) {
+        List<Character> list = new ArrayList<>(letters);
+        if (Character.isAlphabetic(letter))
+            list.add(letter);
+
+        return Builder.from(this)
+                .withLetters(list)
+                .build();
+    }
+
+    public List<Character> getLetters() {
+        return this.letters;
+    }
+
+
+    public static class Builder {
+        private final String word;
+        private List<Character> letters;
+
+        public Builder(String word, List<Character> letters) {
+            this.word = word;
+            this.letters = letters;
+        }
+
+        public static Builder from(Game game) {
+            return new Builder(game.word, game.letters);
+        }
+
+        public static Builder fromWord(String word) {
+            return new Builder(word, emptyList());
+        }
+
+        public Builder withLetters(List<Character> letters) {
+            this.letters = letters;
+            return new Builder(word, letters);
+        }
+
+        public Game build() {
+            return new Game(word, letters);
+        }
+    }
+
+}
---
+++ src/test/java/lt/andriaus/hangman/domain/GameTest.java
@@ -0,0 +1,35 @@
+package lt.andriaus.hangman.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class GameTest {
+
+    @Test
+    void shouldCreateGameWithWord() {
+        String word = "ABC";
+        Game game = Game.Builder.fromWord(word).build();
+
+        String result = game.getWord();
+
+        assertThat(result).isEqualTo(word);
+    }
+
+    @Test
+    void shouldAddLetterOnly() {
+        String word = "ABC";
+        Game game = Game.Builder.fromWord(word).build();
+
+        Game guessedLetter = game.guessLetter('D').guessLetter('5');
+        List<Character> result = guessedLetter.getLetters();
+        assertThat(result).contains('D');
+        assertThat(result).doesNotContain('5');
+    }
+
+}
