81b7c37a45e39fc072582b0c831f617d62e867b6
Andrius Simanaitis <andrius@bei.lt>
2021-02-12 10:20:41 +0200

    git rid of findRegexGroups; did some refactoring

--- src/main/java/lt/andriaus/hangman/util/StringUtils.java
+++ src/main/java/lt/andriaus/hangman/util/StringUtils.java
@@ -1,38 +1,26 @@
 package lt.andriaus.hangman.util;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class StringUtils {
 
-    static public Set<Character> stringToCharSet(String inputString) {
+    public static Set<Character> stringToCharSet(String inputString) {
         return inputString.chars()
                 .mapToObj(e -> (char) e)
                 .collect(Collectors.toSet());
     }
 
-    static public List<Character> stringToCharList(String inputString) {
+    public static List<Character> stringToCharList(String inputString) {
         return inputString.chars()
                 .mapToObj(e -> (char) e)
                 .collect(Collectors.toList());
     }
 
-    static public String charSetToString(Set<Character> inputSet) {
+    public static String charSetToString(Set<Character> inputSet) {
         return inputSet.stream()
                 .map(String::valueOf)
-                .collect(Collectors.joining(""));
-    }
-
-    static public List<String> getRegexGroups(String regexPattern, String string) {
-        List<String> answer = new ArrayList<>();
-        Matcher matcher = Pattern.compile(regexPattern).matcher(string);
-        while (matcher.find())
-            for (int i = 0; i <= matcher.groupCount(); i++)
-                answer.add(matcher.group(i));
-        return answer;
+                .collect(Collectors.joining());
     }
 }
