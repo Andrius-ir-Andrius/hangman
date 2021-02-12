ee8de8d3982dfd26361e1feae341557172dc2452
Andrius Simanaitis <andrius@bei.lt>
2021-02-12 10:52:37 +0200

    Explained the use of JSONGame

--- src/main/java/lt/andriaus/hangman/util/JSONGame.java
+++ src/main/java/lt/andriaus/hangman/util/JSONGame.java
@@ -4,15 +4,22 @@ import lt.andriaus.hangman.domain.Game;
 
 import java.util.Set;
 
-public class JSONGame{
+
+/**
+ * encapsulates Game and is used for turning into json
+ */
+public class JSONGame {
     private final Game game;
-    public JSONGame(Game game){
+
+    public JSONGame(Game game) {
         this.game = game;
     }
-    public Set<Character> getGuessedLetters(){
+
+    public Set<Character> getGuessedLetters() {
         return game.getGuessedLetters();
     }
-    public String getWord(){
+
+    public String getWord() {
         String word = game.getWord();
         Set<Character> letters = game.getGuessedLetters();
         return word.replaceAll(String.format("[^\\s%s]", StringUtils.charSetToString(letters)), "_");
