1c4596cbb077c985403ef769261c7134fd7ad9e2
Andrius Simanaitis <andrius@bei.lt>
2021-02-17 17:15:28 +0200

    added error capturing

--- src/ui/src/App.tsx
+++ src/ui/src/App.tsx
@@ -3,20 +3,32 @@ import CreateButton from "./components/CreateButton";
 import Game from "./domain/Game";
 import GameContext from './domain/GameContext';
 import GuessLetterButton from "./components/GuessLetterButton";
+import {createGame, loadGame} from "./gateway/GameGateway";
 
 
 type stateType = {
     game: Game | null
     isLoaded: boolean
+    error: string | null
 }
 
 function App() {
-    const [state, setState] = useState<stateType>({game: null, isLoaded: false});
+    const [state, setState] = useState<stateType>({game: null, isLoaded: false, error: null});
     const gameContext = useContext(GameContext);
     useEffect(() => {
         (async () => {
             if (gameContext.game === null) {
-                setState({...state, isLoaded: true})
+                const gameId = await createGame().catch((e) => {
+                    setState({...state, error: 'Failed to create game', isLoaded: true})
+                })
+                if(gameId === undefined)
+                    return
+                const game = await loadGame(gameId).catch((e) => {
+                    setState({...state, error: 'Failed to load game', isLoaded: true})
+                })
+                if(game === undefined)
+                    return
+                setState({...state, isLoaded: true, game : game??null})
                 return
             }
             const game = gameContext.game
@@ -38,11 +50,11 @@ function App() {
             <>{!state.isLoaded ? 'Loading' :
                 state.game === null ?
                     <>
-                        <CreateButton/>
+                        {state.error}
                     </> :
                     <>
                         {JSON.stringify(state.game)}
-                        <GuessLetterButton letter={'a'} />
+                        <GuessLetterButton letter={'a'}/>
                     </>
             }</>
         </GameContext.Provider>
